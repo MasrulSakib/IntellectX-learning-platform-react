c308ff1a3c29a5d3b1b68bd091c94f0a55d917b7
MasrulSakib <Masrul.Sakib69@gmail.com>
2024-04-02 06:36:10 +0600

    sign up & sign in UI created

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,26 +4,30 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className="navbar bg-neutral text-neutral-content">
-            <div className="flex-1">
-                <Link className="btn btn-ghost text-2xl antialiased italic bg-black">Intellect<span className=' text-green-500'>X</span></Link>
-            </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li><Link to={'/'}>Home</Link></li>
-                    <li><Link to={'/login'}>Login</Link></li>
-                    <li><Link to={'/register'}>Register</Link></li>
-                    <li>
-                        <details>
-                            <summary>
-                                More
-                            </summary>
-                            <ul className="p-2 bg-base-100 rounded-t-none">
-                                <li><Link >Profile</Link></li>
-                                <li><Link>Link 2</Link></li>
-                            </ul>
-                        </details>
-                    </li>
-                </ul>
+            <div className='container mx-auto'>
+                <div className="flex-1 flex justify-between">
+                    <Link className="btn btn-ghost text-2xl antialiased italic">Intellect<span className=' text-green-500 font-bold'>X</span></Link>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><Link to={'/'}>Home</Link></li>
+                        <li><Link to={'/login'}>Sign in</Link></li>
+                        <li><Link to={'/register'}>Sign up</Link></li>
+                        <li>
+                            <details>
+                                <summary>
+                                    More
+                                </summary>
+                                <ul className="p-2 bg-base-100 rounded-t-none">
+                                    <li><Link >Profile</Link></li>
+                                    <li><Link>Courses</Link></li>
+                                    <li><Link>FAQ</Link></li>
+                                    <li><Link>Blog</Link></li>
+                                </ul>
+                            </details>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
--- src/Components/Routes/Routes.js
+++ src/Components/Routes/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Main/Main";
 import Home from "../Home/Home";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +12,14 @@ export const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
             }
         ]
     }
