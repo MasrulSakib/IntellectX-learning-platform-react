e525dc477d6e8ec959537b3df2c860bdc9ff67a8
MasrulSakib <Masrul.Sakib69@gmail.com>
2024-04-22 12:13:58 +0600

    some minor alignment issues fixed

--- src/Components/Category/Category.js
+++ src/Components/Category/Category.js
@@ -8,7 +8,7 @@ const Category = () => {
     return (
         <div>
             <h2 className='mt-10 mb-6 font-semibold text-2xl'>Total courses in this category: {categoryData.length}</h2>
-            <div className='grid md:grid-cols-2 justify-items-center grid-cols-1 md:gap-20'>
+            <div className='grid md:grid-cols-2 grid-cols-1 justify-items-center md:gap-20 gap-10'>
                 {
                     categoryData.map(courses => <CourseSummary
                         key={courses._id}
--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -10,7 +10,7 @@ const Courses = () => {
     return (
         <div >
             <h2 className='mt-10 mb-6 text-2xl font-semibold'>All Courses:</h2>
-            <div className='grid md:grid-cols-2 grid-cols-1 md:gap-20 gap-10'>
+            <div className='grid md:grid-cols-2 grid-cols-1 justify-items-center md:gap-20 gap-10'>
                 {
                     allCourses.map(courses => <CourseSummary
                         key={courses._id}
